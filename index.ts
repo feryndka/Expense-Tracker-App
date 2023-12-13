cb688066afd0bc327c83e6418e1735e9cedfb0ab
feryndka <feryandika151@gmail.com>
2023-12-13 18:45:50 +0700

    fix(method) : get

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express, { Application } from "express";
+import bodyParser from "body-parser";
 import expenseRouter from "./router/expenseRouter";
 
 const app: Application = express();
 const PORT: number = 4400;
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use("/api/expenses", expenseRouter);
 
 app.listen(PORT, () => {
--- router/expenseRouter.ts
+++ router/expenseRouter.ts
@@ -3,27 +3,74 @@ import axios from "axios";
 
 const router: Router = express.Router();
 
-router.get("/", async (req: Request, res: Response) => {
+router.get("/:id?", async (req: Request, res: Response) => {
   try {
-    const expense = await axios.get("http://localhost:3000/expense");
+    const { id } = req.params || ''
+    const apiUrl = id ? `http://localhost:3000/expenses/${id}` : "http://localhost:3000/expenses"
+    
+    const expenses = await axios.get(apiUrl);
     res.status(200).send({
       message : "success",
-      data : expense.data
+      data : expenses.data
     });
   } catch (err) {
     res.status(500).send({
-      message: err
+      message: JSON.stringify(err)
     });
   }
 });
 
 router.post("/", async (req: Request, res: Response) => {
   try {
-    const expense = await axios.post("http://localhost:3000/expense");
-    res.send(expense.data);
-  } catch (err) {
+    const { name, nominal, category } = req.body
+
+    const expenses = await axios.post("http://localhost:3000/expenses", {
+      name : name,
+      nominal : nominal,
+      category : category
+    });
+    res.status(200).send({
+      message : "success",
+      data : expenses.data
+    });
+  } catch (err : any) {
     res.status(500).send({
-      message: err,
+      message: JSON.stringify(err?.message)
+    });
+  }
+});
+
+router.put("/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const { name, nominal, category } = req.body
+
+    const expenses = await axios.put(`http://localhost:3000/expenses/${id}`, {
+      name, nominal, category
+    });
+    res.status(200).send({
+      message : "success",
+      data : expenses.data
+    });
+  } catch (err : any) {
+    res.status(500).send({
+      message: JSON.stringify(err?.message)
+    });
+  }
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+
+    const expenses = await axios.delete(`http://localhost:3000/expenses/${id}`);
+    res.status(200).send({
+      message : "success",
+      data : expenses.data
+    });
+  } catch (err : any) {
+    res.status(500).send({
+      message: JSON.stringify(err?.message)
     });
   }
 });
